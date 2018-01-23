da96297931c432c05bf387be77b0e860c036ecd9
VB42 <vbhaip01@gmail.com>
2018-01-22 22:28:48 -0500

    Working model ~70% accuracy with 15 epochs, removed dropout and added more layers

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import numpy as np
-from keras.models import Sequential
+from keras.models import Sequential, Model
 from keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D, Activation
 from keras import optimizers
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 import os
 import csv
+from keras.applications.vgg16 import VGG16
+model = VGG16()
 
-width = 356*4
-height = 536*4
+width = 224
+height = 224
 '''
 img = load_img("rim-flow-data/train/glaucoma/G-1-L.jpg")
 x = img_to_array(img) #numpy array
@@ -16,26 +18,40 @@ x = x.reshape((1,) + x.shape) #adds on dimension for keras
 print(x.shape)'''
 
 model = Sequential()
-model.add(Conv2D(4, (10,10), input_shape=(width,height,1)))
+model.add(Conv2D(4, (4,4), input_shape=(width,height,3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(4, 4)))
-model.add(Flatten())
-model.add(Dense(8))
+model.add(Conv2D(8, (3,3)))
 model.add(Activation('relu'))
-model.add(Dropout(0.3))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(16, (2,2)))
+model.add(Activation('relu'))
+model.add(Flatten())
+model.add(Dense(128))
+model.add(Activation('relu'))
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dense(16))
+model.add(Activation('relu'))
 model.add(Dense(2))
 model.add(Activation('softmax'))
 
 #model.load_weights("third_try.h5")
-opt = optimizers.SGD(lr=0.01, momentum=0.5)
+
+#model = VGG16(weights='imagenet', include_top=True)
+#x = Dense(2, activation='softmax', name='predictions')(model.layers[-2].output)
+
+#Then create the corresponding model
+#model = Model(input=model.input, output=x)
+#model.summary()
+
+opt = optimizers.SGD(lr=1, momentum=0.5)
 model.compile(loss='categorical_crossentropy',
-              optimizer='nadam',
+              optimizer='adadelta',
               metrics=['accuracy'])
 
 traindatagen = ImageDataGenerator(
         rescale=1./255,
-        horizontal_flip=True,
-        vertical_flip=True,
         samplewise_std_normalization=True)
 
 testdatagen = ImageDataGenerator(
@@ -45,19 +61,22 @@ testdatagen = ImageDataGenerator(
 train_generator = traindatagen.flow_from_directory(
         'rim-flow-data/train',  # this is the target directory
         target_size=(width, height),
-        batch_size=10,
-        color_mode='grayscale')
+        batch_size=5,
+        color_mode='rgb')
 
 validation_generator = testdatagen.flow_from_directory(
         'rim-flow-data/validation',
         target_size=(width, height),
-        batch_size=10,
-        color_mode='grayscale')
+        color_mode='rgb')
+
 
 model.fit_generator(
         train_generator,
-        epochs=10,
+        epochs=15,
         validation_data=validation_generator)
 
 
-model.save_weights('third_try.h5')
+model.save_weights('sixth_try.h5')
+
+#fifth_try - 61.29%
+#sixth_try - 67%
